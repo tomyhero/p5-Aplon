cfc6adc2cad59d334fb5a2ac816c40e467c40b3b
Tomohiro Teranishi <tomyhero.teranishi@gmail.com>
2011-02-03 12:26:02 +0900

    initial commit

---
+++ lib/App/Logic.pm
@@ -0,0 +1,33 @@
+package App::Logic;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Logic -
+
+=head1 SYNOPSIS
+
+  use App::Logic;
+
+=head1 DESCRIPTION
+
+App::Logic is
+
+=head1 AUTHOR
+
+polocky E<lt>polocky+cpan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
